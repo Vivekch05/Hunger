c87f76a4794468ec462f698d4f617128cc98a218
Vivekch05 <vivek.kch05@gmail.com>
2025-05-04 13:49:17 +0530

    changes for menue item

--- src/Components/Body/Restaurent/index.js
+++ src/Components/Body/Restaurent/index.js
@@ -9,15 +9,12 @@ const Restaurent = () => {
     const handleSearch = () => {
         // Logic to handle search
         const filterRestData = restaurentData?.filter((restaurent) => restaurent?.info?.name?.toLowerCase()?.includes(searchText?.toLowerCase()));
-        console.log(filterRestData);
         setFilteredRestaurentData(filterRestData)
     }
     const fetchData = async () => {
         try {
             const response = await fetch('https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.39140273541051&lng=77.03241761773825&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING')
-            console.log(response);
             const data = await response.json()
-            console.log(data?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
             const restaurentList = data?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
             setRestaurentData(restaurentList);
             setFilteredRestaurentData(restaurentList)
@@ -37,17 +34,16 @@ const Restaurent = () => {
             <div className='flex flex-wrap items-center justify-center overflow-hidden'>
                 {filteredRestaurentData && filteredRestaurentData.map((restaurent, index) => (
                     <Link key={restaurent?.info?.id} to={`/restaurent/${restaurent?.info?.id}`}>
-                        <div key={index} className='w-60 h-80 flex flex-col justify-center border-2 border-gray-300 rounded-lg shadow-lg m-4 p-4'>
-                            <img src={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/${restaurent.info?.cloudinaryImageId}`} alt={restaurent.name} className='w-60 h-40 rounded-lg object-cover' />
-                            <h3 className='text-base font-bold'>{restaurent.info?.name}</h3>
-                            <p className='text-base font-bold'>Status: <span className='text-xs font-bold'>{restaurent?.info?.isOpen ? 'Open' : 'Closed'}</span></p>
-                            <div className='flex justify-between items-center w-full'>
-                                <p>{restaurent?.info?.areaName}</p>
-                                <p>{restaurent?.info?.avgRating}</p>
-                            </div>
-                            <p className='text-xs'>{restaurent?.info?.cuisines?.slice(0, 4)?.join(',')}</p>
-                        </div>
-                    </Link>
+                    <div key={index} className='w-60 h-90 flex flex-col justify-center border-2 border-gray-300 rounded-lg shadow-lg m-4 p-4
+                    transition delay-50 duration-200 ease-in-out hover:-translate-y-1 hover:scale-105 hover:bg-amber-50 hover:shadow-xl'>
+                        <img src={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/${restaurent.info?.cloudinaryImageId}`} alt={restaurent.name} className='w-60 h-40 rounded-lg object-cover' />
+                        <p className='text-base font-bold'>Name: <span className='text-xs text-gray-600'>{restaurent.info?.name}</span></p>
+                        <p className='text-base font-bold'>Status: <span className='text-xs text-gray-600'>{restaurent?.info?.isOpen ? 'Open' : 'Closed'}</span></p>
+                        <p className='text-base font-bold'>Address: <span className='text-xs text-gray-600'>{restaurent?.info?.areaName}, {restaurent?.info?.city}, {restaurent?.info?.locality}</span></p>
+                        <p className='text-base font-bold'>Cuisines: <span className='text-xs text-gray-600'>{restaurent?.info?.cuisines?.slice(0, 3)?.join(',')}....</span></p>
+                        <p className='text-base font-bold'>Rating: ⭐️ <span className='text-xs text-gray-600'>{restaurent?.info?.avgRating}</span></p>
+                    </div>
+                </Link>
                 ))}
             </div>
         </div>
--- src/Components/Body/RestaurentItem/index.js
+++ src/Components/Body/RestaurentItem/index.js
@@ -1,47 +1,51 @@
+
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router';
 const RestaurentItem = () => {
-    const [restaurentItem, setRestaurentItem] = useState({});
     const [restaurentMenu, setRestaurentMenu] = useState([]);
     const params = useParams();
-    console.log(params, 'params');
     const fetchData = async () => {
         try {
-            const response = await fetch(`https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.39211433319374&lng=77.03325261453736&restaurantId=${params?.resId}&catalog_qa=undefined&submitAction=ENTER`);
+            const response = await fetch(`https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.39140273541051&lng=77.03241761773825&restaurantId=${params?.resId}&catalog_qa=undefined&submitAction=ENTER`);
             const data = await response.json();
-            console.log(data, 'restaurentitem');
-            setRestaurentItem(data?.data?.cards[2]?.card?.card?.info);
-            setRestaurentMenu(data?.data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card?.itemCards);
+            const filteredData = data?.data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards?.filter(item => item?.card?.card?.itemCards?.length > 0 ? item?.card?.card : null);
+            await setRestaurentMenu(filteredData);
         } catch (error) {
             console.error('Error fetching data:', error);
         }
     };
-    console.log(restaurentMenu, 'restaurentMenu')
+
     useEffect(() => {
         fetchData();
     }, [])
-
     return (
         <div className='flex flex-col items-center justify-center my-2'>
-            <h1 className='text-2xl font-bold text-red-500'>Restaurent Item</h1>
             {
-                [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((item, index) => (
-                    <div key={index} className='w-4xl h-xl flex flex-row justify-between border-2 border-gray-300 rounded-lg shadow-lg m-4 p-4'>
-                        <div>
-                            <h3 className='text-base font-bold'>{restaurentItem?.name}</h3>
-                            <h4 className='text-base font-bold'>Address: <span className='text-xs text-gray-600'>{restaurentItem?.areaName}, {restaurentItem?.city}, {restaurentItem?.locality}</span></h4>
-                            <h4 className='text-base font-bold'>Rating: <span className='text-xs text-gray-600'>{restaurentItem?.avgRating}</span></h4>
-                            <p className='text-base font-bold'>Cuisines: <span className='text-xs text-gray-600'>{restaurentItem?.cuisines?.slice(0, 4)?.join(',')}</span></p>
-                            <p className='text-base font-bold'>Cost for two: <span className='text-xs text-gray-600'>{restaurentItem?.costForTwoMessage}</span></p>
-                            <p className='text-base font-bold'>Status: <span className='text-xs text-gray-600'>{restaurentItem?.isOpen ? 'Open' : 'Closed'}</span></p>
-                        </div>
-                        <div>
-                            <img className='w-60 h-40 rounded-2xl shadow-2xl' src={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/${restaurentItem?.cloudinaryImageId}`} alt={restaurentItem?.name} />
-                        </div>
-                    </div>))
-            }
-        </div>
-    )
+                restaurentMenu?.length === 0 ? <h1 className='text-2xl font-bold text-red-500'>Loading...</h1> :
+                    restaurentMenu.map((category, categoryIndex) => {
+                        return (
+                            <div key={categoryIndex}>
+                                <h2 className='text-xl font-bold text-black-500'>{category?.card?.card?.title} ({category?.card?.card?.itemCards?.length})</h2>
+                                {category?.card?.card?.itemCards?.map((item, index) => (
+                                    <div key={index} className='w-4xl h-xl flex flex-row justify-between border-1 border-gray-300 rounded-lg shadow-lg m-4 p-4
+                                      transition delay-100 duration-200 ease-in-out hover:-translate-y-1 hover:scale-105 hover:bg-amber-50 hover:shadow-xl'>
+                                        <div>
+                                            <h2>{item?.card?.info?.isVeg ? '🟩' : '🟥'}</h2>
+                                            <h3 className='text-base font-bold'>{item?.card?.info?.name}</h3>
+                                            <p className='text-base font-bold'>Price: <span className='text-xs text-gray-600'>{item?.card?.info?.price ? (item?.card?.info?.price) / 100 : (item?.card?.info?.finalPrice) / 100}</span></p>
+                                            <p className='text-base font-bold'>Rating: ⭐️ <span className='text-xs text-gray-600'>{item?.card?.info?.ratings?.aggregatedRating?.rating}({item?.card?.info?.ratings?.aggregatedRating?.ratingCountV2})</span></p>
+                                            <p className='text-base font-bold'>Description: <span className='text-xs text-gray-600'>{item?.card?.info?.description}</span></p>
+                                            <button className='text-base rounded-lg font-bold p-2 m-1 bg-gray-800 text-white'>Add to Cart</button>
+                                        </div>
+                                        <div>
+                                            <img className='w-60 h-40 rounded-2xl shadow-2xl' src={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/${item?.card?.info?.imageId}`} alt={item?.card?.info?.name} />
+                                        </div>
+                                    </div>
+                                ))}
+                            </div>)
+                    }
+                    )}
+        </div>)
 }
 
 export default RestaurentItem
